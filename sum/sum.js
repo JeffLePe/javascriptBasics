8b06e618a0253ffdb067d78b27b0ba93c1871b08
JeffLePe <jeffersonleon310@yahoo.com.br>
2016-09-03 11:12:27 -0300

    sum with feedback to User

--- sum/sum.js
+++ sum/sum.js
@@ -1,8 +1,17 @@
 var a = process.argv[2];
 var b = process.argv[3];
 
-console.log("Total: " + sum(a, b));
+confirma(a, b);
+
+function confirma(a, b) {
+    if(isNaN(parseFloat(a)) == true || isNaN(parseFloat(b)) == true){
+        console.log("Numero digitado é inválido");
+    }
+    else{
+        console.log("Total: " + sum(a, b));
+    }
+}
 
 function sum(a, b) {
-    return parseInt(a) + parseInt(b);
+    return parseFloat(a) + parseFloat(b);
 }
