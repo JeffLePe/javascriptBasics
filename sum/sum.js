fb8fec2a5537f765eb3068b6a19a90edc6dbeb00
JeffLePe <jeffersonleon310@yahoo.com.br>
2016-09-15 19:22:02 -0300

    new update sum

--- sum/sum.js
+++ sum/sum.js
@@ -1,18 +1,37 @@
-var total = 0;
-
-getNumbers();
-
-console.log("Total: " + total);
+(function showResult() {
+    var numbers = getNumbers();
+    if(numbers.length < 2) {
+        console.log("Please inform at least two valid numbers!");
+    }
+    else {
+        console.log("Total: " + sum(numbers));
+    }
+})();
 
 function getNumbers() {
-    index = 2;
-    process.argv.forEach((val, index) => {
-        if(!isNaN(process.argv[index])) {
-            total = sum(process.argv[index], total);
-        }
-    });
+    var numbers = getArguments()
+                  .filter(isNumber)
+                  .map(stringToNumber);
+    return numbers;
 }
 
-function sum(a, b) {
-    return parseFloat(a) + parseFloat(b);
+function isNumber(value) {
+   return !isNaN(value);
+}
+
+function stringToNumber(stringNumber) {
+    return parseFloat(stringNumber);
+}
+
+function sum(numbers) {
+    var total = 0;
+    var addToTotal = function(number) {
+       total = total + number; 
+    };
+    numbers.forEach(addToTotal);
+    return total;
+}
+
+function getArguments() {
+    return process.argv.slice(2);
 }
