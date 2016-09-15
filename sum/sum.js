6a2a0f539bbe7ea30b6434f973f973a33832d403
JeffLePe <jeffersonleon310@yahoo.com.br>
2016-09-15 18:27:16 -0300

    update sum

--- sum/sum.js
+++ sum/sum.js
@@ -1,12 +1,17 @@
 var total = 0;
 
-process.argv.forEach((val, index) => {
-    if(index > 1) {
-        total = sum(process.argv[index], total);
-    }
-});
+getNumbers();
 
 console.log("Total: " + total);
+
+function getNumbers() {
+    index = 2;
+    process.argv.forEach((val, index) => {
+        if(!isNaN(process.argv[index])) {
+            total = sum(process.argv[index], total);
+        }
+    });
+}
 
 function sum(a, b) {
     return parseFloat(a) + parseFloat(b);
